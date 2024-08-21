19f40e2b32241feb2f852bb7c2d58e0258dd9a7d
Juan Carlos Gomez <juancarlosgomezlopez14@gmail.com>
2024-08-21 22:38:43 +0200

    feat: added sales information cards

--- src/app/control-panel/control-panel.module.ts
+++ src/app/control-panel/control-panel.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarPanelComponent } from './components/navbar-panel/navbar-panel.component';
 import { MaterialModule } from '../material/material.module';
+import { CardsInformationComponent } from './components/cards-information/cards-information.component';
 
 @NgModule({
-  declarations: [NavbarPanelComponent],
+  declarations: [NavbarPanelComponent, CardsInformationComponent],
   imports: [CommonModule, MaterialModule],
-  exports: [NavbarPanelComponent],
+  exports: [NavbarPanelComponent, CardsInformationComponent],
 })
 export class ControlPanelModule {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterHomeComponent } from './components/footer-home/footer-home.component';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
+  exports: [],
 })
 export class HomeModule {}
